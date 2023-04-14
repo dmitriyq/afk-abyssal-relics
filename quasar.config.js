1726d7830f6a7343d67cb94b65c2d15e68cdf265
Borovov Dmitriy <me@dmitriyq.ru>
2023-04-14 12:47:44 +0300

    update netlify config

--- quasar.config.js
+++ quasar.config.js
@@ -61,7 +61,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/ae-relics',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
